75d4767b735cea60ff0e20477c7407a2543f8edb
isaiah dahl <isaiahdahl@me.com>
2017-10-30 12:48:43 +0000

    clean up, nothing major

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,13 @@
 class QuestionsController < ApplicationController
   def answer
+    @question = params[:question]
+    if @question.downcase == "i am going to work right now!"
+      @answer = ""
+    elsif @question.end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "Wrong answer, get dressed and go to work!"
+    end
   end
 
   def ask
